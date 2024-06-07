06f61b4216746e5a273143bab11174d2466daa61
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-07 16:47:20 +0200

    Adjusted my NavLink to properties to use relative links .

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
           <Route path="about" element={<About />}/>
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
 
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />}/>
