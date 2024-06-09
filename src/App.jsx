440b7142640d0c59ed610c939cb6d6a6f3eb0aa9
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-09 23:28:26 +0200

    Imported the AuthRequired  component and wraped my Routes for the host and it's nested routes inside the AuthRequired component route.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 import NotFound from "./pages/NotFound"
 import Login from "./pages/Login"
+import AuthRequired from "./components/AuthRequired"
 
 
 import "./server"
@@ -35,15 +36,17 @@ function App() {
             element={<Login />}
           />
 
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
           </Route>
           
