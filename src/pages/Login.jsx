0102cad764f896adf28434623afdf3360daaec87
Tumelo Mogono <tumelomogono@gmail.com>
2024-06-10 19:40:51 +0200

    imported the loginUser from api. I added two new useState to manage status and error. I then added logic to my handleSubmit function for form submit, which will utilize the loginUser function from api file, then have a then with a catch and finally method to make changes to my setError and setStatus. The error is also used in my rendered elements. The status is also used for my button to toggle between disabled and not and also change the text inside the button.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,26 @@
 import React from "react"
 import { useNavigate, useLocation } from "react-router-dom"
+import { loginUser } from "../api"
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
     const location = useLocation()
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        loginUser(loginFormData)
+        .then(data => {
+            setError(null)
+        })
+        .catch(err => {
+            setError(err)
+        })
+        .finally(() => {
+            setStatus("idle")
+        })
     }
 
     function handleChange(e) {
@@ -20,8 +33,9 @@ export default function Login() {
 
     return (
         <div className="login-container">
-            {location.state?.message && <h3 className="login-first">{location.state.message}</h3>}
+            {location.state?.message && <h3 className="login-error">{location.state.message}</h3>}
             <h1>Sign in to your account</h1>
+            {error?.message && <h3 className="login-error">{error.message}</h3>}
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -37,7 +51,11 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button 
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting" ? "Logging in..." : "Log in"}
+                </button>
             </form>
         </div>
     )
